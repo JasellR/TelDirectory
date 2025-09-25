2880ab32b36016e823d1a272c960d5c7a0c8ec76
Jasell Rojas <rojasjaselwara@gmail.com>
2025-09-25 14:14:45 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import { AppHeader } from '@/components/layout/AppHeader';
 import { PageWrapper } from '@/components/layout/PageWrapper';
@@ -8,23 +7,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { ThemeInitializer } from '@/components/settings/ThemeInitializer';
 import { LanguageProvider } from '@/context/LanguageContext';
 
-
-const geistSans = localFont({
-  src: '../fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-})
-const geistMono = localFont({
-  src: '../fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-})
-
 export const metadata: Metadata = {
   title: 'TelDirectory - Corporate Phone Directory',
   description: 'Find extensions and contact information easily.',
 };
 
-const bodyClassNames = `${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`;
-
+const bodyClassNames = `antialiased flex flex-col min-h-screen`;
 
 export default function RootLayout({
   children,
