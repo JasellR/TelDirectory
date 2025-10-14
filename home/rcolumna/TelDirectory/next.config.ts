738001e7eeaefe4517dfe4b03bce369a01d668f4
Roberto Columna Céspedes <roberto.columnacespedes@gmail.com>
2025-10-14 20:43:45 +0000

    Puedo navegar desde la app, puedo aperturar el xml mediante la web, pero

---
+++ home/rcolumna/TelDirectory/next.config.ts
@@ -0,0 +1,49 @@
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  serverExternalPackages: ['sqlite3', 'ldapjs', 'bcrypt'],
+  
+  // Add CORS headers to allow IP phones to fetch XML files.
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Allow all origins
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, OPTIONS, PATCH, DELETE, POST, PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
